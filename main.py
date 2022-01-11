5aff69410e7e99e5c18d5362b087cc9dd22e0090
WashingtonYandun <washington.yandun06@gmail.com>
2022-01-10 20:33:33 -0500

    start dealing with singly linked list

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+from SinglyLinkedList.Node import Node
+from SinglyLinkedList.SiglyLinkedList import SiglyLinkedList
+
+
 if __name__ == '__main__':
-    print("DSA AND ALGO")
+    # declare the link list
+    listA = SiglyLinkedList()
+
+    # declare the head of the list
+    listA.head = Node(0)
+
+    # declare the elements of the list
+    e1 = Node(1)
+    e2 = Node(2)
+
+    # make the list
+    listA.head.link = e1
+    e1.link = e2
+
+    listA.show()
