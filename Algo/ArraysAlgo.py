c38afea591a718f94a6a3363f3d6db3e87e25356
WashingtonYandun <washington.yandun06@gmail.com>
2022-01-14 09:54:21 -0500

    refactor sum of two function

--- Algo/ArraysAlgo.py
+++ Algo/ArraysAlgo.py
@@ -13,13 +13,21 @@ def sum_of_two_all(arr, value):
     '''
     Print all the matches of the sum_of_two problem. return true if it exist
     '''
-    flag = False
-    for i in arr:
-        for j in range(i+1, len(arr)):
-            if (arr[i]+arr[j] == value):
-                flag = True
-                print([arr[i], arr[j]])
-    return flag
+    cont = 0
+    pairs = {"Pairs": cont}
+
+    for i in range(0, len(arr)):
+        j = i + 1
+        for j in range(j, len(arr)):
+            if (arr[i] + arr[j] == value):
+                cont = cont + 1
+                pairs[cont] = [arr[i], arr[j]]
+
+    pairs["Pairs"] = cont
+    return pairs
 
 
-sum_of_two_all([1, 2, 3, 4, 5, 6], 10)
+print(sum_of_two_all([0, 2, 49, 5, 4, 9, 6, 24, 10,
+      12, -12, 3, 20, 13, 15, 16, 17, 103, 146,
+      134, 126, 167, 190, 90, 80, 70, 30, -57, 29,
+      36, 29, 21, 25, 22, 68, -67, 69, 1], 1))
