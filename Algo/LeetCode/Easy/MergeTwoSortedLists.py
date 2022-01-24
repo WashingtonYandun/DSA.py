04d121cd79030691343f9f85a0548c8b47255b8e
WashingtonYandun <washington.yandun06@gmail.com>
2022-01-23 19:57:18 -0500

    finish merge to lists

--- Algo/LeetCode/Easy/MergeTwoSortedLists.py
+++ Algo/LeetCode/Easy/MergeTwoSortedLists.py
@@ -12,19 +12,35 @@ class ListNode:
         self.val = val
         self.next = next
 
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
 
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None:
-            return list2
-        if list2 is None:
-            return list1
-
-        if list1.val < list2.val:
-            aux = mergedList = ListNode(list1.val)
-            l1 = list1.next
+        if l1.val < l2.val:
+            temp = head = ListNode(l1.val)
+            l1 = l1.next
         else:
-            aux = mergedList = ListNode(list2.val)
-            l2 = list2.next
+            temp = head = ListNode(l2.val)
+            l2 = l2.next
 
-        return mergedList
+        while l1 is not None and l2 is not None:
+            if l1.val < l2.val:
+                temp.next = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                temp.next = ListNode(l2.val)
+                l2 = l2.next
+            temp = temp.next
+
+        while l1 is not None:
+            temp.next = ListNode(l1.val)
+            l1 = l1.next
+            temp = temp.next
+
+        while l2 is not None:
+            temp.next = ListNode(l2.val)
+            l2 = l2.next
+            temp = temp.next
+        return head
