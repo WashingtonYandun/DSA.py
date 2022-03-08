78025dfcb068e9c20ca2deb8bcb5b3805f97d46f
WashingtonYandun <washington.yandun06@gmail.com>
2022-03-07 22:22:47 -0500

    start makin DLL class

---
+++ DS/DoublyLinkedList/DoublyLinkedList.py
@@ -0,0 +1,12 @@
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def show(self):
+        pass
+
+    def insertAtHead(self, newData):
+        pass
+
+    def insertAtLast(self, newData):
+        pass
