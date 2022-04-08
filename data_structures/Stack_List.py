396fd80cd139676c85dc23226105c9d7c9230f1e
WashingtonYandun <washington.yandun06@gmail.com>
2022-04-07 23:33:15 -0500

    stack implementation from built in list in python

---
+++ data_structures/Stack_List.py
@@ -0,0 +1,28 @@
+class Stack_List:
+    stk = []
+
+    def __init__(self):
+        self.stk = []
+
+    def push(self, data):
+        self.stk.append(data)
+
+    def do_pop(self):
+        self.stk.pop()
+
+    def peek(self):
+        return self.stk[-1]
+
+    def size(self):
+        return len(self.stk)
+
+    def empty(self):
+        if self.stk == []:
+            return True
+        return False
+
+    def search(self, data):
+        for i in range(0, self.size(), 1):
+            if self.stk[i] == data:
+                return i
+        return -1
