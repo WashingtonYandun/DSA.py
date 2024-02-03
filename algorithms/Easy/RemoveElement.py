23143d3ab415030c0f3c5293720961a2e0f30364
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-02-03 16:36:31 -0500

    solved: 1 easy problem

---
+++ algorithms/Easy/RemoveElement.py
@@ -0,0 +1,23 @@
+# 27. Remove Element
+'''
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+'''
+
+
+class Solution:
+    # 30ms Beats, 90.84%
+    # 16.60MB, beats 55.12%
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        
+        return k
